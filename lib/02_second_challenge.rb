1d6fbb8b54aa664117dc24bb54b2da5be100eed7
Jzavier Timm <jztimm@gmail.com>
2020-07-02 00:49:31 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,19 +1,18 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {};
-      :matriarch => {};
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
       :hero => {},
       :hero_friends => []
-    }, 
-    :capulet => {
-      :patriarch => {};
-      :matriarch => {};
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-    }
-    
-  }
+   }
+}
 
   
 end
